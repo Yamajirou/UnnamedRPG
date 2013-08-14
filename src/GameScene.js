61e18dbf1c9853b1bde8284a043c0383aeac63af
Yamajirou <sesshoumaru31@gmail.com>
2013-08-14 02:42:23 -0300

    "Menu" de buildings Drag and drop do menu

--- src/GameScene.js
+++ src/GameScene.js
@@ -7,191 +7,291 @@ function isoCoordsForPoint(point, layerSizeAux) {
 
 //http://www.cocos2d-iphone.org/forums/topic/is-there-an-existing-tutorial-for-isometric-game-like-farmville/
 
-	var mapHeight = layerSizeAux.height;
-	var mapWidth = layerSizeAux.width;
-	var tileWidth = 64;
-	var tileHeight = 32;
-	
-	//return pixel coords of grid coords
-	var posY = mapHeight - point.x/tileWidth + mapWidth/2 - point.y/tileHeight;
-	var posX = mapHeight + point.x/tileWidth - mapWidth/2 - point.y/tileHeight;
+    var mapHeight = layerSizeAux.height;
+    var mapWidth = layerSizeAux.width;
+    var tileWidth = 64;
+    var tileHeight = 32;
 
-	if(posX<0)
-		posX=0;
-	if(posY<1)
-		posY=1;
-	if(posX>mapWidth-1)
-		posX=mapWidth-1;
-	if(posY>mapHeight)
-		posY=mapHeight;
-	return (new cc.Point(posX,posY));
-};
+    //return pixel coords of grid coords
+    var posY = mapHeight - point.x / tileWidth + mapWidth / 2 - point.y / tileHeight;
+    var posX = mapHeight + point.x / tileWidth - mapWidth / 2 - point.y / tileHeight;
+
+    if (posX < 0)
+        posX = 0;
+    if (posY < 1)
+        posY = 1;
+    if (posX > mapWidth - 1)
+        posX = mapWidth - 1;
+    if (posY > mapHeight)
+        posY = mapHeight;
+    return (new cc.Point(posX, posY));
+}
+;
 
 
-function tilePosFromLocation(location, tileMap){
+function tilePosFromLocation(location, tileMap) {
 // Tilemap position must be subtracted, in case the tilemap position is scrolling
-var pos = ccpSub(location, tileMap.getPosition());
-var halfMapWidth = tileMap.getMapSize().width * 0.5;
-var mapHeight = tileMap.getMapSize().height;
-var tileWidth = tileMap.getTileSize().width;
-var tileHeight = tileMap.getTileSize().height;
-var tilePosDiv = new cc.Point(pos.x / tileWidth, pos.y / tileHeight);
-var inverseTileY = mapHeight - tilePosDiv.y;
+    var pos = ccpSub(location, tileMap.getPosition());
+    var halfMapWidth = tileMap.getMapSize().width * 0.5;
+    var mapHeight = tileMap.getMapSize().height;
+    var tileWidth = tileMap.getTileSize().width;
+    var tileHeight = tileMap.getTileSize().height;
+    var tilePosDiv = new cc.Point(pos.x / tileWidth, pos.y / tileHeight);
+    var inverseTileY = mapHeight - tilePosDiv.y;
 // Cast to int makes sure that result is in whole numbers
-var posX = (inverseTileY + tilePosDiv.x - halfMapWidth);
-var posY = (inverseTileY - tilePosDiv.x + halfMapWidth);
+    var posX = (inverseTileY + tilePosDiv.x - halfMapWidth);
+    var posY = (inverseTileY - tilePosDiv.x + halfMapWidth);
 // make sure coordinates are within isomap bounds
-posX = MAX(0, posX);
-posX = MIN(tileMap.getMapSize().width - 1, posX);
-posY = MAX(0, posY);
-posY = MIN(tileMap.getMapSize().height - 1, posY);
+    posX = MAX(0, posX);
+    posX = MIN(tileMap.getMapSize().width - 1, posX);
+    posY = MAX(0, posY);
+    posY = MIN(tileMap.getMapSize().height - 1, posY);
 
-posX = Math.floor(posX);
-posY = Math.floor(posY);
+    posX = Math.floor(posX);
+    posY = Math.floor(posY);
 
 
-return new cc.Point(posX, posY);
-};
+    return new cc.Point(posX, posY);
+}
+;
 
-function MAX (a, b){
-	if(a > b)
-		return a;
-	return b;
-};
+function MAX(a, b) {
+    if (a > b)
+        return a;
+    return b;
+}
+;
 
-function MIN (a, b){
-	if(a < b)
-		return a;
-	return b;
-};
+function MIN(a, b) {
+    if (a < b)
+        return a;
+    return b;
+}
+;
 
 function ccpSub(v1, v2)
 {
     return new cc.Point(v1.x - v2.x, v1.y - v2.y);
-};
+}
+;
 
 var HUDLayer = cc.Layer.extend({
-
-	/*
-    isMouseDown:false,
-    helloImg:null,
-    helloLabel:null,
-    circle:null,
-    sprite:null,
-	*/
-	
-    init:function () {
+    _label: null,
+    _mouseDragged: null,
+    _draggingStartPoint: null,
+    _draggingEndPoint: null,
+    _currentGID: null,
+    _tmxMap: null,
+    _menuStartingXOffset: null,
+    _isBuilding: null,
+    _winSize: null,
+    _dragSprite: null,
+    _dragSpriteTag: null,
+    _terrainLayer: "Terrain",
+    _objectLayer: "Objects",
+    
+    init: function(tmxMap) {
         var selfPointer = this;
         //////////////////////////////
         // 1. super init first
         this._super();
-		//this.label = new cc.LabelTTF();
-		
-		//cc.LabelTTF.create(label, fontName, fontSize, dimensions, alignment)
-		//this.label = cc.LabelTTF.create("alignment left", "Verdana", 32, cc.size(winSize.width, 50), cc.TEXT_ALIGNMENT_LEFT);
-		this.label = cc.LabelTTF.create("LAbel", "Verdana", 20, cc.size(100, 100), cc.TEXT_ALIGNMENT_RIGHT);
-		this.label.setPosition(250, 5);
-		this.addChild(this.label);
-		
-		
-		
-		console.log("HUDLayer.init");
-		//console.log(this.isKeyboardEnabled());
-		this.setMouseEnabled(true);
-		this.setKeyboardEnabled(true);
-		console.log("mouse enable: " + this.isMouseEnabled());
-		console.log("keyboard enable: " + this.isKeyboardEnabled());
-	},
-	
-	/*
-	onTouchesMoved: function(pTouch, pEvent){
-		
-		var layerHud = map.getLayer(HUDMapLayer);
-	
-	},
-	
-	*/
-	onMouseUp:function(event){
-		console.log("onMouseUp");
-		this.label.setString(event.getLocation().x + ", " + event.getLocation().y);
-		var pos = event.getLocation();
-		console.log(event.getLocation());
-		
-		var layer2 = map.getLayer(objectLayer);
-		
-		//pos.x < this._layerSize.width && pos.y < this._layerSize.height && pos.x >= 0 && pos.y
-		console.log("pos(" + pos.x + ", " + pos.y + ")");
-		console.log("layer = " + layer2.getLayerSize().width + ", " + layer2.getLayerSize().height);
-		//console.log("layer(" + layer2.width + ", " + layer2.height + ")");
-		
-		var pppp = isoCoordsForPoint(pos, layer2.getLayerSize());
-		console.log("pppp(" + pppp.x + ", " + pppp.y + ")");
-		var tilePosition = tilePosFromLocation(event.getLocation(), map);
-		console.log("tilePosFromLocation = " + tilePosition.x + ", " + tilePosition.y);
-		
-		console.log("tileGITat(" + tilePosition.x + ", " + tilePosition.y + ") = " + layer2.getTileGIDAt(tilePosition));
-		
-		//gid = id do tile; pos = position do tile; flags = ? (numero)
-		//cc.TMXLayer.setTileGID(gid, pos, flags)
-		layer2.setTileGID(121, tilePosition, 0);
-		
-		
-		//console.log("tilePosFromLocation = " + Math.floor(tilePosition.x) + ", " + Math.floor(tilePosition.y));
-		
-		
-		
-		//layer2.removeTileAt(new cc.Point(Math.floor(tilePosition.x), Math.floor(tilePosition.y)));
-		
-		//console.log(layer2.getTileAt(event.getLocation()));
-		//console.log(layer2.getTileAt(165,240));
-	},
+        
+        
+        //this.label = new cc.LabelTTF();
+        this._tmxMap = tmxMap;
+        this._mouseDragged = false;
+        this._draggingStartPoint = cc.p(-1, -1);
+        this._draggingEndPoint = cc.p(-1, -1);
+        this._currentGID = -1;
+        this._isBuilding = false;
+        this._dragSpriteTag = "auxSprite";
+        this._winSize = cc.Director.getInstance().getWinSize();
+        
+        
+        //cc.LabelTTF.create(label, fontName, fontSize, dimensions, alignment)
+        //this.label = cc.LabelTTF.create("alignment left", "Verdana", 32, cc.size(winSize.width, 50), cc.TEXT_ALIGNMENT_LEFT);
+        this._label = cc.LabelTTF.create("Label", "Verdana", 20, cc.size(100, 100), cc.TEXT_ALIGNMENT_RIGHT);
+        this._label.setPosition(250, 5);
+        this.addChild(this._label);
 
-	onKeyUp:function(keyCode){
-		
-		if(keyCode == cc.KEY.space){
-			var layer1 = map.getLayer(layerName);
-			var w = layer1.getLayerSize().width;
-			var h = layer1.getLayerSize().height;
-			
-			for(var px = 0; px < w; px++){
-				for(var py = 0; py < h; py++){
-					var p = layer1.getPositionAt(new cc.Point(px,py))
-					console.log("tile(" + px + ", " + py + ") = " + "position(" + p.x + ", " + p.y + ")" );
-				}
-			}
-		}
-	}
-	
-	
-	
+        // ----- Menu ------
+        var menuSprite1 = cc.Sprite.create(s_isotile, cc.rect((64*1), 64 * 12, 64, 64));
+        var menuSprite2 = cc.Sprite.create(s_isotile, cc.rect((64*4), 64 * 12, 64, 64));
+        var menuSprite3 = cc.Sprite.create(s_isotile, cc.rect((64*9), 64 * 12, 64, 64));
+        
+//        var menuSpriteSelected1 = cc.Sprite.create(s_isotile, cc.rect(0,64*12+1,64,64));
+//        var menuItem1 = cc.MenuItemSprite.create(menuSprite1, menuSpriteSelected1, this.menuItemFunc, this);
+//        var menu = cc.Menu.create(menuItem1);
+////        var menu = cc.Menu.create();
+//        menu.alignItemsVertically();
+        this._menuStartingXOffset = this._winSize.width - 100;
+        
+        
+//        menu.setPosition(cc.p(winSize.width - 100, winSize.height - 150));
+//        this.addChild(menu);
+
+        menuSprite1.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 150));
+        menuSprite2.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 200));
+        menuSprite3.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 250));
+        this.addChild(menuSprite1);
+        this.addChild(menuSprite2);
+        this.addChild(menuSprite3);
+
+        // --- enabling mouse and keyboard -----
+        //console.log(this.isKeyboardEnabled());
+        this.setMouseEnabled(true);
+        this.setKeyboardEnabled(true);
+//        this.setTouchEnabled(true);
+        console.log("mouse enable: " + this.isMouseEnabled());
+        console.log("keyboard enable: " + this.isKeyboardEnabled());
+    },
+            
+    onMouseDown: function(event) {
+        
+        if (!this.mouseDragged) {
+            if(event.getLocation().x > this._menuStartingXOffset){
+                //dragging from menu
+                this._isBuilding = true;
+                
+                console.log("height = " + this._winSize.height);
+                
+                //case with menu options
+                if (event.getLocation().y <= this._winSize.height - 150 && 
+                        event.getLocation().y > this._winSize.height - 200){
+                    // sprite 1 - gid=122
+                    this._currentGID = 122;
+                    this._dragSprite = cc.Sprite.create(s_isotile, cc.rect((64*1), 64 * 12, 64, 64));
+                    this._dragSprite.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 150));
+                }else if (event.getLocation().y <= this._winSize.height - 200 && 
+                        event.getLocation().y > this._winSize.height - 250){
+                    // sprite 2 - gid=125
+                    this._currentGID = 125;
+                    this._dragSprite = cc.Sprite.create(s_isotile, cc.rect((64*4), 64 * 12, 64, 64));
+                    this._dragSprite.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 200));
+                }else if (event.getLocation().y <= this._winSize.height - 250 && 
+                        event.getLocation().y > this._winSize.height - 300){
+                    // sprite 3 - gid=130
+                    this._currentGID = 130;
+                    this._dragSprite = cc.Sprite.create(s_isotile, cc.rect((64*9), 64 * 12, 64, 64));
+                    this._dragSprite.setPosition(cc.p(this._winSize.width - 100, this._winSize.height - 250));
+                }
+                console.log("_currentGID = " + this._currentGID);
+                this.addChild(this._dragSprite, 10, this._dragSpriteTag);
+            }
+            //TODO check the dragging sprite
+            this._draggingStartPoint = event.getLocation();
+        }
+    },
+            
+    onMouseDragged: function(event) {
+        this._mouseDragged = true;
+        if(this._isBuilding){
+            this._dragSprite.setPosition(event.getLocation());
+        }
+        //TODO if(isBuilding) { follow_mouse_with_sprite = true}
+        //TODO if(GID != -1){ GID = checkspriteGID()}
+        console.log("onMouseDragged");
+    },
+            
+    /*
+     menuItemFunc: function(){
+     console.log("menuItemFunc");
+     },
+
+    onTouchesMoved: function(pTouch, pEvent) {
+        //        var layerHud = map.getLayer(HUDMapLayer);
+        console.log("onTouchesMoved");
+
+    },
+     */
+            
+    onMouseUp: function(event) {
+        console.log("onMouseUp");
+        this._label.setString(event.getLocation().x + ", " + event.getLocation().y);
+        var pos = event.getLocation();
+        console.log(event.getLocation());
+
+        var layer2 = this._tmxMap.getLayer(this._objectLayer);
+
+        //pos.x < this._layerSize.width && pos.y < this._layerSize.height && pos.x >= 0 && pos.y
+        console.log("pos(" + pos.x + ", " + pos.y + ")");
+        console.log("layer = " + layer2.getLayerSize().width + ", " + layer2.getLayerSize().height);
+        //console.log("layer(" + layer2.width + ", " + layer2.height + ")");
+
+        var pppp = isoCoordsForPoint(pos, layer2.getLayerSize());
+        console.log("pppp(" + pppp.x + ", " + pppp.y + ")");
+        var tilePosition = tilePosFromLocation(event.getLocation(), this._tmxMap);
+        console.log("tilePosFromLocation = " + tilePosition.x + ", " + tilePosition.y);
+
+        console.log("tileGITat(" + tilePosition.x + ", " + tilePosition.y + ") = " + layer2.getTileGIDAt(tilePosition));
+        if (this._mouseDragged && this._isBuilding) {
+            if(this._currentGID !== -1){
+                this._draggingEndPoint = tilePosFromLocation(event.getLocation(), this._tmxMap);
+                //TODO put sprite, change GID
+
+                //gid = id do tile; pos = position do tile; flags = ? (numero)
+                //cc.TMXLayer.setTileGID(gid, pos, flags)
+                layer2.setTileGID(this._currentGID, this._draggingEndPoint, 0);
+                this._currentGID = -1;
+            }
+        }
+        this.removeChild(this._dragSprite, true);
+        this._mouseDragged = false;
+        this._isBuilding = false;
+        //console.log("tilePosFromLocation = " + Math.floor(tilePosition.x) + ", " + Math.floor(tilePosition.y));
+        //layer2.removeTileAt(new cc.Point(Math.floor(tilePosition.x), Math.floor(tilePosition.y)));
+        //console.log(layer2.getTileAt(event.getLocation()));
+        //console.log(layer2.getTileAt(165,240));
+    },
+            
+    onKeyUp: function(keyCode) {
+        if (keyCode === cc.KEY.space) {
+            var layer1 = this._tmxMap.getLayer(this._terrainLayer);
+            var w = layer1.getLayerSize().width;
+            var h = layer1.getLayerSize().height;
+
+            for (var px = 0; px < w; px++) {
+                for (var py = 0; py < h; py++) {
+                    var p = layer1.getPositionAt(new cc.Point(px, py));
+                    console.log("tile(" + px + ", " + py + ") = " + "position(" + p.x + ", " + p.y + ")");
+                }
+            }
+        }
+    }
 });
 
 
 var GameScene = cc.Scene.extend({
-    onEnter:function () {
+    onEnter: function() {
         this._super();
+
+        //var map = cc.TMXTiledMap.create("../mapas/map/mapa1.tmx", "../mapas/map/TileA2.png");
+        this.map = new cc.TMXTiledMap();
+        this.map.initWithTMXFile(s_SmallMap);
+
         var layer = new HUDLayer();
-        layer.init();
-		
-		//var map = cc.TMXTiledMap.create("../mapas/map/mapa1.tmx", "../mapas/map/TileA2.png");
-		map = new cc.TMXTiledMap()
-		map.initWithTMXFile(s_SmallMap);
-		layerName = "Terrain";
-		objectLayer = "Objects";
-		//HUDMapLayer = "HUDLayer";
-		//console.log("childs = " + map.getChildrenCount());
-		
-		
-		
-		this.addChild(map);
-		console.log ("asd");
-		
-		
+        layer.init(this.map);
+        
+        /*
+        var backgroundLayer = new cc.Layer.create();
+        backgroundLayer.init();
+        var winSize = 640;
+        var placeMenuItem1 = cc.Sprite.create(s_tileBuilding, cc.rect((64*0), 64 * 3, 64, 64));
+        var placeMenuItem2 = cc.Sprite.create(s_tileBuilding, cc.rect((64*0), 64 * 3, 64, 64));
+        var placeMenuItem3 = cc.Sprite.create(s_tileBuilding, cc.rect((64*0), 64 * 3, 64, 64));
+        placeMenuItem1.setPosition(cc.p(winSize.width - 100, winSize.height - 150));
+        placeMenuItem2.setPosition(cc.p(winSize.width - 100, winSize.height - 200));
+        placeMenuItem3.setPosition(cc.p(winSize.width - 100, winSize.height - 250));
+        backgroundLayer.addChild(placeMenuItem1);
+        backgroundLayer.addChild(placeMenuItem2);
+        backgroundLayer.addChild(placeMenuItem3);
+        this.addChild(backgroundLayer);
+        */
+        
+        this.addChild(this.map);
+        console.log("asd");
+
         this.addChild(layer);
     }
-	
+
 });
-
-
-
-
--- src/resource.js
+++ src/resource.js
@@ -5,22 +5,24 @@ var s_Map = "mapas/map/isomap1.tmx";
 var s_SmallMap = "mapas/map/smallIsoMap1.tmx";
 var s_TileA2 = "mapas/map/TileA2.png";
 var s_isotile = "mapas/map/IsometricTerrainTileset.png";
+var s_tileBuilding = "mapas/map/iso-64x64-building_2.png";
 
 var g_resources = [
     //image
     {src:s_HelloWorld},
     {src:s_CloseNormal},
     {src:s_CloseSelected},
-	{src:s_TileA2},
-	{src:s_isotile},
+    {src:s_TileA2},
+    {src:s_isotile},
+    {src:s_tileBuilding},
 	
     //plist
 
     //fnt
 
     //tmx
-	{type:"tmx", src:s_SmallMap},
-	{type:"tmx", src:s_Map}
+    {type:"tmx", src:s_SmallMap},
+    {type:"tmx", src:s_Map}
 
     //bgm
 
